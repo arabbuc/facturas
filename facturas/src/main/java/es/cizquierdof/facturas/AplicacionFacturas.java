5690944123aee6bf37fcb909e3689653fe045fca
WEB <cizquierdof@gmail.com>
2020-02-19 09:48:07 +0100

    proyecto inicial

---
+++ facturas/src/main/java/es/cizquierdof/facturas/AplicacionFacturas.java
@@ -0,0 +1,13 @@
+package es.cizquierdof.facturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionFacturas {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionFacturas.class, args);
+	}
+
+}
