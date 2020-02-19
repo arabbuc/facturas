c9cf7d1ad39c23b53ab233fcf63af20156cb6dc9
WEB <cizquierdof@gmail.com>
2020-02-19 14:00:38 +0100

    ++

--- facturas/src/main/java/es/cizquierdof/facturas/controller/ProductoRestController.java
+++ facturas/src/main/java/es/cizquierdof/facturas/controller/ProductoRestController.java
@@ -5,26 +5,40 @@ import org.springframework.web.bind.annotation.RestController;
 import es.cizquierdof.facturas.model.Producto;
 import es.cizquierdof.facturas.repositorio.ProductoRepository;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
 
 /**
  * ProductoRestController
  */
 
 @RestController
-@RequestMapping(value="/productos")
+@RequestMapping(value = "/productos")
 public class ProductoRestController {
 
     @Autowired
     ProductoRepository pr;
 
     @GetMapping("/")
-    public Iterable<Producto> getAllProductos(){
+    public Iterable<Producto> getAllProductos() {
         return pr.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Producto> getProducto(
+        @PathVariable("id") Long id
+    ){
+        try {
+            return pr.findById(id);
+        } catch (Exception e) {
+            
+            return null;
+        }
+        
     }
 
     
--- facturas/src/main/java/es/cizquierdof/facturas/repositorio/ProductoRepository.java
+++ facturas/src/main/java/es/cizquierdof/facturas/repositorio/ProductoRepository.java
@@ -1,5 +1,7 @@
 package es.cizquierdof.facturas.repositorio;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import es.cizquierdof.facturas.model.Producto;
@@ -8,5 +10,7 @@ import es.cizquierdof.facturas.model.Producto;
  * ProductoRepository
  */
 public interface ProductoRepository extends CrudRepository<Producto, Long> {
+
+    List<Producto> findByDescripcion(String descripcion);
   
 }
